a8fdd2cf3ba12c96c7382ecae7ff3a721670ecd6
Ivan G <160726846+IvanGarbuz@users.noreply.github.com>
2026-09-04 21:04:43 +0400

    cleanup and configuration (#15)
    Co-authored-by: PenguinSatanJr <penguinsatan107@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
-
 @Module({
   imports: [],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces/index.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    routeConflictPolicy: { duplicate: 'error', shadow: 'warn' },
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  });
 }
 await bootstrap();
