a780b1633c31744eec38c320514fefa7538211bc
PenguinSatanJr <penguinsatan107@gmail.com>
2026-09-04 20:58:44 +0400

    cleanup and configuration

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: {
-    globals: true,
-    root: './',
-    include: ['**/*.e2e-spec.ts'],
-  },
 });
